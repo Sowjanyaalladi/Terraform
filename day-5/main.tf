ab09ff35b187cab041cf28ea513a7fcd11db150a
root <root@ip-10-0-16-11.ec2.internal>
2026-06-06 06:58:13 +0000

    push into the git from ec2

--- day-5/main.tf
+++ day-5/main.tf
@@ -7,8 +7,16 @@ resource "aws_vpc" "name" {
 }
 resource "aws_subnet" "name" {
     vpc_id     = aws_vpc.name.id
+<<<<<<< HEAD
     cidr_block = "10.0.0.0/26"
     tags = {
         Name = "Terraform-0900AM"
     }
 }
+=======
+    cidr_block = "10.0.0.64/26"
+    tags = {
+        Name = "Terraform-0900AM"
+    }
+}
+>>>>>>> 779bc20 (day5)

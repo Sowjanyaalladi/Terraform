6eee560c0cf20f45168492a03383df0e9bbd193e
Sowjanyaalladi <sowjialladi@gmail.com>
2026-06-10 16:21:21 +0530

    Add TERRAFORM FILES

--- day-3/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "name" {
-    ami           = "ami-00e801948462f718a"
-    instance_type = "t2.medium"
-    tags = {
-        Name = "keerthi"
-    }
-} 
--- day2/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ami_id = "ami-00e801948462f718a"
-instance_type = "t2.medium"
-name = "my-ec2-instance"

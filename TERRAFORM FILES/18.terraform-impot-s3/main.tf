fd8db23ce4d95fb302491ec5078a4717bc7e0d36
Sowjanyaalladi <sowjialladi@gmail.com>
2026-06-11 13:03:07 +0530

    terraform

---
+++ TERRAFORM FILES/18.terraform-impot-s3/main.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "name" {
+    bucket = "my-s3-buckettttttttttt"
+   
+  
+}
+resource "aws_s3_bucket_versioning" "name" {
+    bucket = aws_s3_bucket.name.id
+    versioning_configuration {
+        status = "Suspended"
+    }
+}
